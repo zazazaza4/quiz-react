48f6fa04d39c346827c9b801b8c733b15726d3f3
Yaroslav <6715414@gmail.com>
2024-02-17 17:44:29 +0200

    feat: add PageError  widget

---
+++ src/widgets/PageError/ui/PageError.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { PageError } from './PageError';
+
+const meta = {
+  title: 'widget/PageError',
+  component: PageError,
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof PageError>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {};
---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import { classNames } from '@/shared/lib/classNames/classNames';
+import { Button } from '@/shared/ui/Button';
+import { VStack } from '@/shared/ui/Stack';
+import { Text } from '@/shared/ui/Text';
+
+import cls from './PageError.module.scss';
+
+interface PageErrorProps {
+  className?: string;
+}
+
+export const PageError: FC<PageErrorProps> = ({ className }) => {
+  const { t } = useTranslation();
+
+  const reloadPage = () => {
+    window.location.reload();
+  };
+
+  return (
+    <VStack gap="16" className={classNames(cls.PageError, {}, [className])}>
+      <Text title={t('error.message')} />
+      <Button onClick={reloadPage}>{t('error.button')}</Button>
+    </VStack>
+  );
+};
