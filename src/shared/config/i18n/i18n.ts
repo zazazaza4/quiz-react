6c37a73f2ad5fae9c7a632311f04a5511ea69a16
Yaroslav <6715414@gmail.com>
2024-02-18 16:05:55 +0200

    feat: add config for i18n

---
+++ src/shared/config/i18n/i18n.ts
@@ -0,0 +1,25 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend';
+import { initReactI18next } from 'react-i18next';
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    debug: __IS_DEV__,
+    cleanCode: true,
+    nonExplicitSupportedLngs: true,
+    backend: {
+      loadPath: 'locales/{{lng}}/{{ns}}.json',
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+    supportedLngs: ['en', 'fr', 'de', 'es'],
+    load: 'languageOnly',
+  });
+
+export default i18n;
