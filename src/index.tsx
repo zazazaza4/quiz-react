78e3097b5bca43acb1066f022831d988a9be8c07
Yaroslav <6715414@gmail.com>
2024-02-18 19:16:27 +0200

    feat: add providers and App component to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
+import App from '@/app/App';
+import { ErrorBoundary } from '@/app/providers/ErrorBoundary';
+import { StoreProvider } from '@/app/providers/StoreProvider';
+
+import '@/shared/config/i18n/i18n';
+
+import '@/app/styles/index.scss';
+
 const container = document.getElementById('root');
 if (!container) {
   throw new Error('Container root was not found');
@@ -9,6 +17,10 @@ if (!container) {
 const root = createRoot(container);
 root.render(
   <BrowserRouter>
-    <div>Start</div>
+    <StoreProvider>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </StoreProvider>
   </BrowserRouter>
 );
