89ebf08eeda862313356f210fab40d0b43f2ba32
Yaroslav <6715414@gmail.com>
2024-02-18 19:15:09 +0200

    chore: add router consts

---
+++ src/shared/const/router.ts
@@ -0,0 +1,13 @@
+export enum AppRoutes {
+  EMAIL = 'email',
+  QUIZ = 'quiz',
+  RESULT = 'result',
+  LOADING = 'loading',
+
+  NOT_FOUND = 'not_found',
+}
+
+export const getRouteEmail = () => '/email';
+export const getRouteQuiz = (id: string) => `/quiz/${id}`;
+export const getRouteResult = () => `/result`;
+export const getRouteLoading = () => `/loading`;
