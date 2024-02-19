3caceae080949964fbff53664e4630554e3395dc
Yaroslav <6715414@gmail.com>
2024-02-19 02:16:04 +0200

    refactor: QuizContent component for better usability and performance

--- src/entities/Quiz/ui/QuizContent/QuizContent.tsx
+++ src/entities/Quiz/ui/QuizContent/QuizContent.tsx
@@ -1,4 +1,4 @@
-import { FC, memo, useMemo } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import { StateSchema } from '@/app/providers/StoreProvider';
@@ -16,50 +16,77 @@ import { QuizSingleSelectImage } from '../QuizSingleSelectImage/QuizSingleSelect
 
 import cls from './QuizContent.module.scss';
 
-interface QuizContentProps {
+interface QuizContentProps<T> {
   className?: string;
   quizId: string;
-  onNext: (nextId?: number) => void;
+  onNext: (value: T, nextId?: number) => void;
 }
 
-export const QuizContent: FC<QuizContentProps> = memo(
-  (props: QuizContentProps) => {
-    const { className, quizId, onNext } = props;
-    const quizItem = useSelector((state: StateSchema) =>
-      getQuizList.selectById(state, quizId)
-    );
+export const QuizContent = <T extends any>(props: QuizContentProps<T>) => {
+  const { className, quizId, onNext } = props;
+  const quizItem = useSelector((state: StateSchema) =>
+    getQuizList.selectById(state, quizId)
+  );
 
-    const content = useMemo(() => {
-      switch (quizItem?.type) {
-        case QuizType.SINGLE_SELECT:
-          return <QuizSingleSelect item={quizItem} onNext={onNext} />;
-        case QuizType.SINGLE_SELECT_IMAGE:
-          return <QuizSingleSelectImage item={quizItem} onNext={onNext} />;
-        case QuizType.BUBBLE:
-          return <QuizBubbleSelect item={quizItem} onNext={onNext} />;
-        case QuizType.MULTIPLE_SELECT:
-          return <QuizMultipleSelect item={quizItem} onNext={onNext} />;
-        default:
-          return null;
-      }
-    }, [onNext, quizItem]);
+  const [isVisible, setIsVisible] = useState(true);
 
-    return (
-      <VStack
-        max
-        align="center"
-        className={classNames(cls.QuizContent, {}, [className])}
-      >
-        <VStack gap="16" className={cls.question} align="center">
-          <Text align="center" title={quizItem?.question} />
-          {quizItem?.description && (
-            <Text align="center" theme="gray" text={quizItem?.description} />
-          )}
-        </VStack>
-        <VStack className={cls.content} max>
-          {content}
-        </VStack>
+  const handleNext = useCallback(
+    (value: string, nextId?: number) => {
+      setIsVisible(false);
+      setTimeout(() => {
+        const result = {
+          order: quizItem?.id,
+          title: quizItem?.question,
+          type: quizItem?.type,
+          answer: value,
+        } as T;
+
+        onNext(result, nextId);
+        setIsVisible(true);
+      }, 500);
+    },
+    [onNext, quizItem?.id, quizItem?.question, quizItem?.type]
+  );
+
+  const content = useMemo(() => {
+    switch (quizItem?.type) {
+      case QuizType.SINGLE_SELECT:
+        return <QuizSingleSelect item={quizItem} onNext={handleNext} />;
+      case QuizType.SINGLE_SELECT_IMAGE:
+        return <QuizSingleSelectImage item={quizItem} onNext={handleNext} />;
+      case QuizType.BUBBLE:
+        return <QuizBubbleSelect item={quizItem} onNext={handleNext} />;
+      case QuizType.MULTIPLE_SELECT:
+        return <QuizMultipleSelect item={quizItem} onNext={handleNext} />;
+      default:
+        return null;
+    }
+  }, [handleNext, quizItem]);
+
+  return (
+    <VStack
+      max
+      align="center"
+      className={classNames(cls.QuizContent, {}, [className])}
+    >
+      <VStack gap="16" className={cls.question} align="center">
+        <Text align="center" title={quizItem?.question} />
+        {quizItem?.description && (
+          <Text align="center" theme="gray" text={quizItem?.description} />
+        )}
       </VStack>
-    );
-  }
-);
+      <VStack
+        className={classNames(
+          cls.content,
+          {
+            [cls.fadeOut]: !isVisible,
+          },
+          []
+        )}
+        max
+      >
+        {content}
+      </VStack>
+    </VStack>
+  );
+};
